54a5fc6261b4f28818efc082feaf65029a6c8579
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-29 13:43:12 +0000

    Added some basic block classes

---
+++ src/main/java/com/mjr/mjrlegendslib/block/BlockBasic.java
@@ -0,0 +1,13 @@
+package com.mjr.mjrlegendslib.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockBasic extends Block {
+
+	public BlockBasic(Material material) {
+		super(material);
+		this.setHarvestLevel("pickaxe", 1);
+	}
+
+}
---
+++ src/main/java/com/mjr/mjrlegendslib/block/BlockBasicGravel.java
@@ -0,0 +1,21 @@
+package com.mjr.mjrlegendslib.block;
+
+import java.util.Random;
+
+import net.minecraft.block.BlockFalling;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+
+public class BlockBasicGravel extends BlockFalling {
+	public BlockBasicGravel(String name) {
+		super();
+		this.setHardness(0.6F);
+		this.setUnlocalizedName(name);
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return Items.FLINT;
+	}
+}
