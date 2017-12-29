a8521b1210e54007c631e9141c88d8e98805dc80
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-29 15:49:53 +0000

    Fixed dependency version on forge

--- src/main/java/com/mjr/mjrlegendslib/Constants.java
+++ src/main/java/com/mjr/mjrlegendslib/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 	public static final String modVersion = LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
 
 	public static final String MCVERSION = "[1.11.2]";
-	public static final String DEPENDENCIES_FORGE = "required-after:Forge@[13.20.1.2513,); "; // Keep the space at the end!
+	public static final String DEPENDENCIES_FORGE = "required-after:forge@[13.20.1.2513,); "; // Keep the space at the end!
 	public static final String DEPENDENCIES_MODS = " "; // Keep the space at the end!
 
 	public static final String ASSET_PREFIX = modID;
