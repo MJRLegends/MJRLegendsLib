b332f57cb41a39842faaca396fde02a3d1f22890
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-29 20:42:32 +0000

    Released v1.0.1

--- src/main/java/com/mjr/mjrlegendslib/Constants.java
+++ src/main/java/com/mjr/mjrlegendslib/Constants.java
@@ -6,11 +6,11 @@ public class Constants {
 
 	public static final int LOCALMAJVERSION = 1;
 	public static final int LOCALMINVERSION = 0;
-	public static final int LOCALBUILDVERSION = 0;
+	public static final int LOCALBUILDVERSION = 1;
 
 	public static final String MCVERSION = "[1.10.2]";
 	
-	public static final String modVersion = MCVERSION + "-" + LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
+	public static final String modVersion = "1.10.2" + "-" + LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
 	
 	public static final String DEPENDENCIES_FORGE = "required-after:Forge@[12.18.3.2239,); "; // Keep the space at the end!
 	public static final String DEPENDENCIES_MODS = " "; // Keep the space at the end!
