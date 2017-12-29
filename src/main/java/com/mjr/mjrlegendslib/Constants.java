bd1339e96845bfffbda0f21973fb420da7a03f56
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-29 12:56:16 +0000

    Initial commit

---
+++ src/main/java/com/mjr/mjrlegendslib/Constants.java
@@ -0,0 +1,33 @@
+package com.mjr.mjrlegendslib;
+
+public class Constants {
+	public static final String modID = "mjrlegendslib";
+	public static final String modName = "MJRLegendsLib";
+
+	public static final int LOCALMAJVERSION = 0;
+	public static final int LOCALMINVERSION = 0;
+	public static final int LOCALBUILDVERSION = 1;
+
+	public static final String modVersion = LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
+
+	public static final String MCVERSION = "[1.10.2]";
+	public static final String DEPENDENCIES_FORGE = "required-after:Forge@[12.18.3.2239,); "; // Keep the space at the end!
+	public static final String DEPENDENCIES_MODS = " "; // Keep the space at the end!
+
+	public static final String ASSET_PREFIX = modID;
+	public static final String TEXTURE_PREFIX = ASSET_PREFIX + ":";
+	public static final String PREFIX = modID + ".";
+
+	public static final float RADIANS_TO_DEGREES = 180F / 3.1415927F;
+	public static final double RADIANS_TO_DEGREES_D = 180D / Math.PI;
+
+	public static final float twoPI = Constants.floatPI * 2F;
+	public static final float halfPI = Constants.floatPI / 2F;
+	public static final float quarterPI = Constants.halfPI / 2F;
+	public static final float floatPI = 3.1415927F;
+
+	public static final String CONFIG_FILE = "config/MJRLegendsLib.cfg";
+
+	public static final String CONFIG_CATEGORY_COMPATIBILITY = "compatibility support";
+	public static final String CONFIG_CATEGORY_GENERAL_SETTINGS = "general settings";
+}
---
+++ src/main/java/com/mjr/mjrlegendslib/MJRLegendsLib.java
@@ -0,0 +1,30 @@
+package com.mjr.mjrlegendslib;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Constants.modID, name = Constants.modName, version = Constants.modVersion, dependencies = Constants.DEPENDENCIES_FORGE + Constants.DEPENDENCIES_MODS)
+public class MJRLegendsLib
+{
+	@Instance(Constants.modID)
+	public static MJRLegendsLib instance;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void postinit(FMLPostInitializationEvent event) {
+
+	}
+}
