5e7878113e431e68480ed9c97553a3dcc39fa724
MJRLegends <MJRLegends@users.noreply.github.com>
2017-12-29 20:22:38 +0000

    Added MC Version to file name

--- src/main/java/com/mjr/mjrlegendslib/Constants.java
+++ src/main/java/com/mjr/mjrlegendslib/Constants.java
@@ -8,9 +8,10 @@ public class Constants {
 	public static final int LOCALMINVERSION = 0;
 	public static final int LOCALBUILDVERSION = 1;
 
-	public static final String modVersion = LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
-
 	public static final String MCVERSION = "[1.11.2]";
+	
+	public static final String modVersion = MCVERSION + "-" + LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
+
 	public static final String DEPENDENCIES_FORGE = "required-after:forge@[13.20.1.2513,); "; // Keep the space at the end!
 	public static final String DEPENDENCIES_MODS = " "; // Keep the space at the end!
 
