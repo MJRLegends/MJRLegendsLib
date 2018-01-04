14ba6a80a353910a40bddac21684edf362359eb0
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-04 01:40:33 +0000

    Added back a method in Client Utilities

---
+++ src/main/java/com/mjr/mjrlegendslib/client/model/ModelTransformWrapper.java
@@ -0,0 +1,36 @@
+package com.mjr.mjrlegendslib.client.model;
+
+import java.lang.reflect.Field;
+
+import javax.vecmath.Matrix4f;
+
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
+import net.minecraftforge.client.model.PerspectiveMapWrapper;
+import net.minecraftforge.common.model.TRSRTransformation;
+
+import com.google.common.collect.ImmutableMap;
+
+/*
+ * Class from Galacticraft Core
+ * Credit micdoodle8, radfast
+ */
+
+abstract public class ModelTransformWrapper extends PerspectiveMapWrapper {
+	public ModelTransformWrapper(IBakedModel parent) {
+		super(parent, (ImmutableMap<TransformType, TRSRTransformation>) null);
+		ImmutableMap.Builder<TransformType, TRSRTransformation> buildIt = ImmutableMap.builder();
+		for (TransformType tt : TransformType.values()) {
+			Matrix4f m = this.getTransformForPerspective(tt);
+			buildIt.put(tt, TRSRTransformation.blockCenterToCorner(new TRSRTransformation(m)));
+		}
+		try {
+			Field f = this.getClass().getSuperclass().getDeclaredField("transforms");
+			f.set(this, buildIt.build());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	abstract protected Matrix4f getTransformForPerspective(TransformType cameraTransformType);
+}
--- src/main/java/com/mjr/mjrlegendslib/util/ClientUtilities.java
+++ src/main/java/com/mjr/mjrlegendslib/util/ClientUtilities.java
@@ -1,11 +1,26 @@
 package com.mjr.mjrlegendslib.util;
 
+import java.util.List;
+
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.client.model.obj.OBJModel;
+import net.minecraftforge.common.model.IModelState;
+import net.minecraftforge.common.model.TRSRTransformation;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableMap;
+import com.mjr.mjrlegendslib.Constants;
+import com.mjr.mjrlegendslib.client.model.ModelTransformWrapper;
 
 public class ClientUtilities {
 	public static void addVariants(String modID, String name, String... variants) {
@@ -35,5 +50,41 @@ public class ClientUtilities {
 
 	public static void registerItemJson(String texturePrefix, Item item, int meta, String name) {
 		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, meta, new ModelResourceLocation(texturePrefix + name, "inventory"));
+	}
+
+	public static void replaceModelDefault(String modID, ModelBakeEvent event, String resLoc, String objLoc, List<String> visibleGroups, Class<? extends ModelTransformWrapper> clazz, IModelState parentState, String... variants) {
+		if (variants.length == 0) {
+			variants = new String[] { "inventory" };
+		}
+
+		OBJModel model;
+
+		try {
+			model = (OBJModel) ModelLoaderRegistry.getModel(new ResourceLocation(modID, objLoc));
+			model = (OBJModel) model.process(ImmutableMap.of("flip-v", "true"));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		Function<ResourceLocation, TextureAtlasSprite> spriteFunction = location -> Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(location.toString());
+		for (String variant : variants) {
+			ModelResourceLocation modelResourceLocation = new ModelResourceLocation(modID + ":" + resLoc, variant);
+			IBakedModel object = event.getModelRegistry().getObject(modelResourceLocation);
+			if (object != null) {
+				if (!variant.equals("inventory"))
+					parentState = TRSRTransformation.identity();
+
+				IBakedModel newModel = model.bake(new OBJModel.OBJState(visibleGroups, false, parentState), DefaultVertexFormats.ITEM, spriteFunction);
+				if (clazz != null) {
+					try {
+						newModel = clazz.getConstructor(IBakedModel.class).newInstance(newModel);
+					} catch (Exception e) {
+						MessageUtilities.fatalErrorMessageToLog(Constants.modID, "ItemModel constructor problem for " + modelResourceLocation);
+						e.printStackTrace();
+					}
+				}
+				event.getModelRegistry().putObject(modelResourceLocation, newModel);
+			}
+		}
 	}
 }
