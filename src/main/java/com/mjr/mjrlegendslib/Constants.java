c3c3242fe694c318f853e94d5bacb5020b9c8f46
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-07 00:54:24 +0000

    Change to Forge 1.12.2-14.23.1.2555

--- src/main/java/com/mjr/mjrlegendslib/Constants.java
+++ src/main/java/com/mjr/mjrlegendslib/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
 
 	public static final String modVersion = "1.12.2" + "-" + LOCALMAJVERSION + "." + LOCALMINVERSION + "." + LOCALBUILDVERSION;
 
-	public static final String DEPENDENCIES_FORGE = "required-after:forge@[14.23.1.2566,); "; // Keep the space at the end!
+	public static final String DEPENDENCIES_FORGE = "required-after:forge@[14.23.1.2555,); "; // Keep the space at the end!
 	public static final String DEPENDENCIES_MODS = " "; // Keep the space at the end!
 
 	public static final String ASSET_PREFIX = modID;
