4be836358d6919a38aa008c9d5ca1c0e107d94d4
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-08 23:08:53 +0000

    *Added BiomeDecoratorBase class *Fix formatting

--- src/main/java/com/mjr/mjrlegendslib/util/RecipeUtilities.java
+++ src/main/java/com/mjr/mjrlegendslib/util/RecipeUtilities.java
@@ -6,8 +6,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class RecipeUtilities {
-    public static void addSmelting(@Nonnull ItemStack input, @Nonnull ItemStack output, float xp)
-    {
-    	GameRegistry.addSmelting(input, output, xp);
-    }
+	public static void addSmelting(@Nonnull ItemStack input, @Nonnull ItemStack output, float xp) {
+		GameRegistry.addSmelting(input, output, xp);
+	}
 }
