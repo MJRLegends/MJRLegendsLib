e4b0e41a77fc895c166e1e54ae5da543848341f2
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-08 04:09:01 +0000

    Added a Recipe Utilities class

---
+++ src/main/java/com/mjr/mjrlegendslib/util/RecipeUtilities.java
@@ -0,0 +1,13 @@
+package com.mjr.mjrlegendslib.util;
+
+import javax.annotation.Nonnull;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class RecipeUtilities {
+    public static void addSmelting(@Nonnull ItemStack input, @Nonnull ItemStack output, float xp)
+    {
+    	GameRegistry.addSmelting(input, output, xp);
+    }
+}
