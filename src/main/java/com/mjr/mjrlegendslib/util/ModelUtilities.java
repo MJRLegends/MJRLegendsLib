5bfea29a46233ce29b208fe367363912eaa328ac
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-08 21:52:48 +0000

    Fixes for MC 1.12.2

--- src/main/java/com/mjr/mjrlegendslib/util/ModelUtilities.java
+++ src/main/java/com/mjr/mjrlegendslib/util/ModelUtilities.java
@@ -1,32 +1,33 @@
 package com.mjr.mjrlegendslib.util;
 
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraftforge.client.model.IFlexibleBakedModel;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 import org.lwjgl.opengl.GL11;
 
 public class ModelUtilities {
 	// Credit micdoodle8, radfast
-	public static void drawBakedModel(IFlexibleBakedModel model) {
+	public static void drawBakedModel(IBakedModel model) {
 		Tessellator tessellator = Tessellator.getInstance();
-		WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-		worldrenderer.begin(GL11.GL_QUADS, model.getFormat());
+		BufferBuilder worldrenderer = tessellator.getBuffer();
+		worldrenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.ITEM);
 
-		for (BakedQuad bakedquad : model.getGeneralQuads()) {
+		for (BakedQuad bakedquad : model.getQuads(null, null, 0)) {
 			worldrenderer.addVertexData(bakedquad.getVertexData());
 		}
 
 		tessellator.draw();
 	}
 
-	public static void drawBakedModelColored(IFlexibleBakedModel model, int color) {
+	public static void drawBakedModelColored(IBakedModel model, int color) {
 		Tessellator tessellator = Tessellator.getInstance();
-		WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-		worldrenderer.begin(GL11.GL_QUADS, model.getFormat());
+		BufferBuilder worldrenderer = tessellator.getBuffer();
+		worldrenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.ITEM);
 
-		for (BakedQuad bakedquad : model.getGeneralQuads()) {
+		for (BakedQuad bakedquad : model.getQuads(null, null, 0)) {
 			int[] data = bakedquad.getVertexData();
 			data[3] = color;
 			data[10] = color;
--- src/main/java/com/mjr/mjrlegendslib/util/TranslateUtilities.java
+++ src/main/java/com/mjr/mjrlegendslib/util/TranslateUtilities.java
@@ -3,20 +3,19 @@ package com.mjr.mjrlegendslib.util;
 import java.util.Arrays;
 import java.util.List;
 
-import net.minecraft.util.StatCollector;
+import net.minecraft.util.text.translation.I18n;
 
-import com.mjr.mjrlegendslib.Constants;
-
+@SuppressWarnings("deprecation")
 public class TranslateUtilities {
 	// Credit micdoodle8, radfast
 	public static String translate(String key) {
-		String result = StatCollector.translateToLocal(key);
+		String result = I18n.translateToLocal(key);
 		int comment = result.indexOf('#');
 		String ret = (comment > 0) ? result.substring(0, comment).trim() : result;
 		for (int i = 0; i < key.length(); ++i) {
 			Character c = key.charAt(i);
 			if (Character.isUpperCase(c)) {
-				MessageUtilities.fatalErrorMessageToLog(Constants.modID, ret);
+				System.err.println(ret);
 			}
 		}
 		return ret;
@@ -30,13 +29,13 @@ public class TranslateUtilities {
 	}
 
 	public static String translateWithFormat(String key, Object... values) {
-		String result = StatCollector.translateToLocalFormatted(key, values);
+		String result = I18n.translateToLocalFormatted(key, values);
 		int comment = result.indexOf('#');
 		String ret = (comment > 0) ? result.substring(0, comment).trim() : result;
 		for (int i = 0; i < key.length(); ++i) {
 			Character c = key.charAt(i);
 			if (Character.isUpperCase(c)) {
-				MessageUtilities.fatalErrorMessageToLog(Constants.modID, ret);
+				System.err.println(ret);
 			}
 		}
 		return ret;
