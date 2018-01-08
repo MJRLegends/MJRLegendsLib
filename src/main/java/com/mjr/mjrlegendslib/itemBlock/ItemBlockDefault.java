e0a74e7b8e164e0f90a59a2866f59f3fba912962
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-08 04:09:23 +0000

    Added a Item Block Default class

---
+++ src/main/java/com/mjr/mjrlegendslib/itemBlock/ItemBlockDefault.java
@@ -0,0 +1,11 @@
+package com.mjr.mjrlegendslib.itemBlock;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+
+public class ItemBlockDefault extends ItemBlock{
+    public ItemBlockDefault(Block block)
+    {
+        super(block);
+    }
+}
