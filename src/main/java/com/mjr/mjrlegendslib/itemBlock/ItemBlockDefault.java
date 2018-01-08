0c8cba4499fbdc2e575df63fbc966275e17b284b
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-08 23:08:41 +0000

    *Added BiomeDecoratorBase class *Fix formatting

--- src/main/java/com/mjr/mjrlegendslib/itemBlock/ItemBlockDefault.java
+++ src/main/java/com/mjr/mjrlegendslib/itemBlock/ItemBlockDefault.java
@@ -3,9 +3,8 @@ package com.mjr.mjrlegendslib.itemBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
 
-public class ItemBlockDefault extends ItemBlock{
-    public ItemBlockDefault(Block block)
-    {
-        super(block);
-    }
+public class ItemBlockDefault extends ItemBlock {
+	public ItemBlockDefault(Block block) {
+		super(block);
+	}
 }
