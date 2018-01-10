9fd83e9866f7e75ab5b0db6207a6d8ff0901271c
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-10 00:53:31 +0000

    Added BasicItem class

---
+++ src/main/java/com/mjr/mjrlegendslib/item/BasicItem.java
@@ -0,0 +1,10 @@
+package com.mjr.mjrlegendslib.item;
+
+import net.minecraft.item.Item;
+
+public class BasicItem extends Item{
+
+	public BasicItem(String name){
+		this.setUnlocalizedName(name);
+	}
+}
