477902d32939b5137c57691244c4cf307aa1421c
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-22 15:32:07 +0000

    Added MappingUtilities, Added more methods to MessageUtilities

---
+++ src/main/java/com/mjr/mjrlegendslib/util/MappingUtilities.java
@@ -0,0 +1,34 @@
+package com.mjr.mjrlegendslib.util;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+
+public class MappingUtilities {
+	public static void remapBlock(String modID, RegistryEvent.MissingMappings<Block> event, String nameOld, Block block) {
+		for (RegistryEvent.MissingMappings.Mapping<Block> mappings : event.getMappings()) {
+			if (mappings.key.getResourceDomain().equals(modID) && mappings.key.getResourcePath().equals(nameOld)) {
+				mappings.remap(block);
+				MessageUtilities.infoMessageToLog(modID, "Remapping Block Complete (From {} to {})", mappings.key, block.getRegistryName());
+			}
+		}
+	}
+
+	public static void remapItem(String modID, RegistryEvent.MissingMappings<Item> event, String nameOld, Block block) {
+		for (RegistryEvent.MissingMappings.Mapping<Item> mappings : event.getMappings()) {
+			if (mappings.key.getResourceDomain().equals(modID) && mappings.key.getResourcePath().equals(nameOld)) {
+				mappings.remap(Item.getItemFromBlock(block));
+				MessageUtilities.infoMessageToLog(modID, "Remapping Block Complete (From {} to {})", mappings.key, block.getRegistryName());
+			}
+		}
+	}
+
+	public static void remapItem(String modID, RegistryEvent.MissingMappings<Item> event, String nameOld, Item item) {
+		for (RegistryEvent.MissingMappings.Mapping<Item> mappings : event.getMappings()) {
+			if (mappings.key.getResourceDomain().equals(modID) && mappings.key.getResourcePath().equals(nameOld)) {
+				mappings.remap(item);
+				MessageUtilities.infoMessageToLog(modID, "Remapping Block Complete (From {} to {})", mappings.key, item.getRegistryName());
+			}
+		}
+	}
+}
