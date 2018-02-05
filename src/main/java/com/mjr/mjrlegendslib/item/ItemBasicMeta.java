52d551c697a3444aa01dbd76aa3e6e1bba18b3b9
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-05 16:25:01 +0000

    Added ItemBasicMeta class

---
+++ src/main/java/com/mjr/mjrlegendslib/item/ItemBasicMeta.java
@@ -0,0 +1,35 @@
+package com.mjr.mjrlegendslib.item;
+
+import java.util.List;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public abstract class ItemBasicMeta extends BasicItem {
+
+	public ItemBasicMeta(String name) {
+		super(name);
+		this.setMaxDamage(0);
+		this.setHasSubtypes(true);
+	}
+
+	@Override
+	public String getUnlocalizedName(ItemStack itemStack) {
+		return this.getUnlocalizedName() + "." + getItemList()[itemStack.getItemDamage()];
+	}
+
+	@Override
+	public void getSubItems(Item par1, CreativeTabs par2CreativeTabs, List<ItemStack> par3List) {
+		for (int i = 0; i < getItemList().length; i++) {
+			par3List.add(new ItemStack(par1, 1, i));
+		}
+	}
+
+	@Override
+	public int getMetadata(int par1) {
+		return par1;
+	}
+
+	public abstract String[] getItemList();
+}
