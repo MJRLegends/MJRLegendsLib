9d2eed3a1cfe5c36590a842fcfddaa46b51e363b
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-09 22:42:41 +0000

    Added one method to TranslateUtilities class

--- src/main/java/com/mjr/mjrlegendslib/util/TranslateUtilities.java
+++ src/main/java/com/mjr/mjrlegendslib/util/TranslateUtilities.java
@@ -10,6 +10,20 @@ import com.mjr.mjrlegendslib.Constants;
 @SuppressWarnings("deprecation")
 public class TranslateUtilities {
 	// Credit micdoodle8, radfast
+	
+	public static String translate(String key, boolean logError) {
+		String result = I18n.translateToLocal(key);
+		int comment = result.indexOf('#');
+		String ret = (comment > 0) ? result.substring(0, comment).trim() : result;
+		for (int i = 0; i < key.length(); ++i) {
+			Character c = key.charAt(i);
+			if (logError && Character.isUpperCase(c)) {
+				MessageUtilities.fatalErrorMessageToLog(Constants.modID, ret);
+			}
+		}
+		return ret;
+	}
+	
 	public static String translate(String key) {
 		String result = I18n.translateToLocal(key);
 		int comment = result.indexOf('#');
