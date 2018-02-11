04c011613c1341167d83516edafcdb44bb82a990
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-11 17:13:21 +0000

    Added Fingerprint Violation Jar Signing

--- src/main/java/com/mjr/mjrlegendslib/MJRLegendsLib.java
+++ src/main/java/com/mjr/mjrlegendslib/MJRLegendsLib.java
@@ -3,11 +3,14 @@ package com.mjr.mjrlegendslib;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Constants.modID, name = Constants.modName, version = Constants.modVersion, dependencies = Constants.DEPENDENCIES_FORGE + Constants.DEPENDENCIES_MODS)
+import com.mjr.mjrlegendslib.util.MessageUtilities;
+
+@Mod(modid = Constants.modID, name = Constants.modName, version = Constants.modVersion, dependencies = Constants.DEPENDENCIES_FORGE + Constants.DEPENDENCIES_MODS, certificateFingerprint = Constants.CERTIFICATEFINGERPRINT)
 public class MJRLegendsLib {
 	@Instance(Constants.modID)
 	public static MJRLegendsLib instance;
@@ -25,5 +28,10 @@ public class MJRLegendsLib {
 	@EventHandler
 	public void postinit(FMLPostInitializationEvent event) {
 
+	}
+
+	@EventHandler
+	public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+		MessageUtilities.fatalErrorMessageToLog(Constants.modID, "Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported!");
 	}
 }
