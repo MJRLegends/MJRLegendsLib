d275548c0d93e7cf7e3b73776f7b80620cff2f13
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-11 02:29:46 +0000

    Added IInventoryDefaults class

---
+++ src/main/java/com/mjr/mjrlegendslib/inventory/IInventoryDefaults.java
@@ -0,0 +1,45 @@
+package com.mjr.mjrlegendslib.inventory;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.util.IChatComponent;
+
+public interface IInventoryDefaults extends IInventory {
+	@Override
+	public default void openInventory(EntityPlayer player) {
+	}
+
+	@Override
+	public default void closeInventory(EntityPlayer player) {
+	}
+
+	@Override
+	public default int getField(int id) {
+		return 0;
+	}
+
+	@Override
+	public default void setField(int id, int value) {
+	}
+
+	@Override
+	public default int getFieldCount() {
+		return 0;
+	}
+
+	@Override
+	public default void clear() {
+
+	}
+
+	// Override & return true if your using getName() method
+	@Override
+	public default boolean hasCustomName() {
+		return false;
+	}
+
+	@Override
+	public default IChatComponent getDisplayName() {
+		return null;
+	}
+}
