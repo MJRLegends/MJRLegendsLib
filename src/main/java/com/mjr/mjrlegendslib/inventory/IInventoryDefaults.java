f41bc0ae469fdb417ce083499184cd39c444a994
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-11 03:30:32 +0000

    *Fixed null pointer on tile entities for getDisplayName

--- src/main/java/com/mjr/mjrlegendslib/inventory/IInventoryDefaults.java
+++ src/main/java/com/mjr/mjrlegendslib/inventory/IInventoryDefaults.java
@@ -2,6 +2,8 @@ package com.mjr.mjrlegendslib.inventory;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.IChatComponent;
 
 public interface IInventoryDefaults extends IInventory {
@@ -40,6 +42,5 @@ public interface IInventoryDefaults extends IInventory {
 
 	@Override
 	public default IChatComponent getDisplayName() {
-		return null;
-	}
+		return (this.hasCustomName() ? new ChatComponentText(this.getName()) : new ChatComponentTranslation(this.getName(), new Object[0]));	}
 }
