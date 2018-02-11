23cc6a0253d20796fcf1633db6bf1852779e4983
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-11 03:12:01 +0000

    Added more methods to Network & Player Utilities classses

--- src/main/java/com/mjr/mjrlegendslib/util/NetworkUtilities.java
+++ src/main/java/com/mjr/mjrlegendslib/util/NetworkUtilities.java
@@ -1,10 +1,25 @@
 package com.mjr.mjrlegendslib.util;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.network.INetHandler;
+import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
 public class NetworkUtilities {
 	public static void registerGuiHandler(Object mod, IGuiHandler handler){
 		NetworkRegistry.INSTANCE.registerGuiHandler(mod, handler);
+    }
+	
+    public static EntityPlayer getPlayerFromNetHandler(INetHandler handler)
+    {
+        if (handler instanceof NetHandlerPlayServer)
+        {
+            return ((NetHandlerPlayServer) handler).playerEntity;
+        }
+        else
+        {
+            return null;
+        }
     }
 }
--- src/main/java/com/mjr/mjrlegendslib/util/PlayerUtilties.java
+++ src/main/java/com/mjr/mjrlegendslib/util/PlayerUtilties.java
@@ -2,7 +2,9 @@ package com.mjr.mjrlegendslib.util;
 
 import java.util.UUID;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class PlayerUtilties {
@@ -13,5 +15,9 @@ public class PlayerUtilties {
 
 	public static EntityPlayerMP getPlayerFromUUID(UUID player) {
 		return (EntityPlayerMP) FMLCommonHandler.instance().getMinecraftServerInstance().getEntityFromUuid(player);
+	}
+
+	public static void sendMessage(EntityPlayer player, String message) {
+		player.addChatMessage(new ChatComponentText(message));
 	}
 }
