8066b077357832214140f7cfbc1a2eca0480c851
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-11 15:52:42 +0000

    Changes to ItemBasicMeta class

--- src/main/java/com/mjr/mjrlegendslib/item/ItemBasicMeta.java
+++ src/main/java/com/mjr/mjrlegendslib/item/ItemBasicMeta.java
@@ -6,21 +6,24 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 
 public abstract class ItemBasicMeta extends BasicItem {
+	private String[] items;
 
-	public ItemBasicMeta(String name) {
+	public ItemBasicMeta(String name, CreativeTabs tab, String[] itemsTemp) {
 		super(name);
 		this.setMaxDamage(0);
 		this.setHasSubtypes(true);
+		this.setCreativeTab(tab);
+		items = itemsTemp;
 	}
 
 	@Override
 	public String getUnlocalizedName(ItemStack itemStack) {
-		return this.getUnlocalizedName() + "." + getItemList()[itemStack.getItemDamage()];
+		return this.getUnlocalizedName() + "." + items[itemStack.getItemDamage()];
 	}
 
 	@Override
 	public void getSubItems(Item par1, CreativeTabs tab, NonNullList<ItemStack> par3List) {
-		for (int i = 0; i < getItemList().length; i++) {
+		for (int i = 0; i < items.length; i++) {
 			par3List.add(new ItemStack(par1, 1, i));
 		}
 	}
@@ -29,6 +32,4 @@ public abstract class ItemBasicMeta extends BasicItem {
 	public int getMetadata(int par1) {
 		return par1;
 	}
-
-	public abstract String[] getItemList();
 }
