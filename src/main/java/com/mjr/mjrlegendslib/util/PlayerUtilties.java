c17ed43e55a42d48a5caf0acf037106712a9dddf
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-11 03:31:49 +0000

    Few fixes MC 1.11+

--- src/main/java/com/mjr/mjrlegendslib/util/PlayerUtilties.java
+++ src/main/java/com/mjr/mjrlegendslib/util/PlayerUtilties.java
@@ -22,6 +22,6 @@ public class PlayerUtilties {
 	}
 
 	public static void sendMessage(EntityPlayer player, String message) {
-		player.addChatMessage(new TextComponentString(message));
+		player.sendMessage(new TextComponentString(message));
 	}
 }
