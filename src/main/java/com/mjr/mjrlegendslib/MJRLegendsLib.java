af22627c10a8af702ae5c74ec9ec00be4960840b
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-14 15:54:28 +0000

    Added Custom OBJ Loader to fix resource leak in vanilla Forge when loading OBJ models.

--- src/main/java/com/mjr/mjrlegendslib/MJRLegendsLib.java
+++ src/main/java/com/mjr/mjrlegendslib/MJRLegendsLib.java
@@ -1,7 +1,6 @@
 package com.mjr.mjrlegendslib;
 
-import com.mjr.mjrlegendslib.util.MessageUtilities;
-
+import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -10,6 +9,9 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import com.mjr.mjrlegendslib.client.model.OBJLoaderCustom;
+import com.mjr.mjrlegendslib.util.MessageUtilities;
+
 @Mod(modid = Constants.modID, name = Constants.modName, version = Constants.modVersion, dependencies = Constants.DEPENDENCIES_FORGE + Constants.DEPENDENCIES_MODS, certificateFingerprint = Constants.CERTIFICATEFINGERPRINT)
 public class MJRLegendsLib {
 	@Instance(Constants.modID)
@@ -17,6 +19,7 @@ public class MJRLegendsLib {
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
+		ModelLoaderRegistry.registerLoader(OBJLoaderCustom.instance);
 	}
 
 	@EventHandler
@@ -26,7 +29,7 @@ public class MJRLegendsLib {
 	@EventHandler
 	public void postinit(FMLPostInitializationEvent event) {
 	}
-	
+
 	@EventHandler
 	public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
 		MessageUtilities.fatalErrorMessageToLog(Constants.modID, "Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported!");
