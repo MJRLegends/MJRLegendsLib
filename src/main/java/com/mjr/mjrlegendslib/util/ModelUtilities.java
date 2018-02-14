180d956cb297c6d107924620bf9054c64bfe2dbf
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-14 15:54:54 +0000

    Added some more method to ModelUtilities class

--- src/main/java/com/mjr/mjrlegendslib/util/ModelUtilities.java
+++ src/main/java/com/mjr/mjrlegendslib/util/ModelUtilities.java
@@ -1,11 +1,27 @@
 package com.mjr.mjrlegendslib.util;
 
+import java.io.IOException;
+import java.util.List;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.resources.model.IBakedModel;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.IFlexibleBakedModel;
+import net.minecraftforge.client.model.IModel;
+import net.minecraftforge.client.model.IModelState;
+import net.minecraftforge.client.model.TRSRTransformation;
+import net.minecraftforge.client.model.obj.OBJModel;
 
 import org.lwjgl.opengl.GL11;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import com.mjr.mjrlegendslib.client.model.OBJLoaderCustom;
 
 public class ModelUtilities {
 	// Credit micdoodle8, radfast
@@ -36,5 +52,19 @@ public class ModelUtilities {
 		}
 
 		tessellator.draw();
+	}
+
+	public static IFlexibleBakedModel modelFromOBJ(ResourceLocation loc) throws IOException {
+		return (IFlexibleBakedModel) modelFromOBJ(loc, ImmutableList.of("main"));
+	}
+
+	public static IFlexibleBakedModel modelFromOBJ(ResourceLocation loc, List<String> visibleGroups) throws IOException {
+		return (IFlexibleBakedModel) modelFromOBJ(loc, visibleGroups, TRSRTransformation.identity());
+	}
+
+	public static IBakedModel modelFromOBJ(ResourceLocation loc, List<String> visibleGroups, IModelState parentState) throws IOException {
+		IModel model = OBJLoaderCustom.instance.loadModel(loc);
+		Function<ResourceLocation, TextureAtlasSprite> spriteFunction = location -> Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(location.toString());
+		return model.bake(new OBJModel.OBJState(visibleGroups, false, parentState), DefaultVertexFormats.ITEM, spriteFunction);
 	}
 }
