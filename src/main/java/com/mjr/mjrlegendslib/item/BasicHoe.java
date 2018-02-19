79436596849bce26e53b230de1cf46763de59d53
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-19 00:09:38 +0000

    Tweaks to Basic Tool classes

--- src/main/java/com/mjr/mjrlegendslib/item/BasicHoe.java
+++ src/main/java/com/mjr/mjrlegendslib/item/BasicHoe.java
@@ -3,8 +3,8 @@ package com.mjr.mjrlegendslib.item;
 import net.minecraft.item.ItemHoe;
 
 public class BasicHoe extends ItemHoe {
-	public BasicHoe(ToolMaterial p_i45347_1_, String name) {
-		super(p_i45347_1_);
+	public BasicHoe(ToolMaterial material, String name) {
+		super(material);
 		this.setUnlocalizedName(name);
 	}
 }
--- src/main/java/com/mjr/mjrlegendslib/item/BasicPickaxe.java
+++ src/main/java/com/mjr/mjrlegendslib/item/BasicPickaxe.java
@@ -3,8 +3,9 @@ package com.mjr.mjrlegendslib.item;
 import net.minecraft.item.ItemPickaxe;
 
 public class BasicPickaxe extends ItemPickaxe {
-	public BasicPickaxe(ToolMaterial p_i45347_1_, String name) {
-		super(p_i45347_1_);
+
+	public BasicPickaxe(ToolMaterial material, String name) {
+		super(material);
 		this.setUnlocalizedName(name);
 	}
 }
