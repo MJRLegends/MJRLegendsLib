2cbb3edf0cb01caf91f810bebffca05b5655150b
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-19 00:07:02 +0000

    Tweaks to Basic Tool classes

--- src/main/java/com/mjr/mjrlegendslib/item/BasicAxe.java
+++ src/main/java/com/mjr/mjrlegendslib/item/BasicAxe.java
@@ -3,8 +3,9 @@ package com.mjr.mjrlegendslib.item;
 import net.minecraft.item.ItemAxe;
 
 public class BasicAxe extends ItemAxe {
-	public BasicAxe(ToolMaterial p_i45347_1_, String name) {
-		super(p_i45347_1_);
+
+	public BasicAxe(ToolMaterial material, String name) {
+		super(material);
 		this.setUnlocalizedName(name);
 	}
 }
--- src/main/java/com/mjr/mjrlegendslib/item/BasicSword.java
+++ src/main/java/com/mjr/mjrlegendslib/item/BasicSword.java
@@ -1,10 +1,31 @@
 package com.mjr.mjrlegendslib.item;
 
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.item.ItemSword;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
 public class BasicSword extends ItemSword {
-	public BasicSword(ToolMaterial p_i45347_1_, String name) {
-		super(p_i45347_1_);
+
+	private final double attackDamageBase;
+
+	public BasicSword(ToolMaterial material, String name, double damageBase) {
+		super(material);
+		this.attackDamageBase = damageBase + material.getDamageVsEntity();
 		this.setUnlocalizedName(name);
+	}
+
+	@Override
+	public float getDamageVsEntity() {
+		return (float) attackDamageBase;
+	}
+
+	@Override
+	public Multimap<String, AttributeModifier> getItemAttributeModifiers() {
+		Multimap<String, AttributeModifier> multimap = HashMultimap.create();
+		multimap.put(SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName(), new AttributeModifier(itemModifierUUID, "Weapon modifier", this.attackDamageBase, 0));
+		return multimap;
 	}
 }
