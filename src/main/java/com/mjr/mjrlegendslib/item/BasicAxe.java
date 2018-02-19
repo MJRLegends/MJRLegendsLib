095fee949779587747d252f9809441d75ecaeef6
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-19 00:09:38 +0000

    Tweaks to Basic Tool classes

--- src/main/java/com/mjr/mjrlegendslib/item/BasicAxe.java
+++ src/main/java/com/mjr/mjrlegendslib/item/BasicAxe.java
@@ -12,8 +12,8 @@ public class BasicAxe extends ItemTool {
 	private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(new Block[] { Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON,
 			Blocks.WOODEN_PRESSURE_PLATE });
 
-	public BasicAxe(ToolMaterial p_i45347_1_, String name) {
-		super(p_i45347_1_, EFFECTIVE_ON);
+	public BasicAxe(ToolMaterial material, String name) {
+		super(material, EFFECTIVE_ON);
 		this.setUnlocalizedName(name);
 	}
 }
