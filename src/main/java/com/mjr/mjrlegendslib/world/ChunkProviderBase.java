f2f0a6bd9b2e3d9b6f375fd6d8fd03b5546510d2
MJRLegends <MJRLegends@users.noreply.github.com>
2018-02-25 17:51:23 +0000

    Clean up/Fixed formatting

--- src/main/java/com/mjr/mjrlegendslib/world/ChunkProviderBase.java
+++ src/main/java/com/mjr/mjrlegendslib/world/ChunkProviderBase.java
@@ -6,66 +6,55 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
 
-public abstract class ChunkProviderBase implements IChunkProvider
-{
-    @Override
-    public BlockPos getStrongholdGen(World worldIn, String structureName, BlockPos position)
-    {
-        return null;
-    }
+public abstract class ChunkProviderBase implements IChunkProvider {
+	@Override
+	public BlockPos getStrongholdGen(World worldIn, String structureName, BlockPos position) {
+		return null;
+	}
 
-    @Override
-    public Chunk provideChunk(BlockPos blockPosIn)
-    {
-        return this.provideChunk(blockPosIn.getX() >> 4, blockPosIn.getZ() >> 4);
-    }
+	@Override
+	public Chunk provideChunk(BlockPos blockPosIn) {
+		return this.provideChunk(blockPosIn.getX() >> 4, blockPosIn.getZ() >> 4);
+	}
 
-    @Override
-    // This is something connected with Ocean Monuments - not used in Space dimensions
-    public boolean func_177460_a(IChunkProvider p_177460_1_, Chunk p_177460_2_, int p_177460_3_, int p_177460_4_)
-    {
-        return false;
-    }
+	@Override
+	// This is something connected with Ocean Monuments - not used in Space dimensions
+	public boolean func_177460_a(IChunkProvider p_177460_1_, Chunk p_177460_2_, int p_177460_3_, int p_177460_4_) {
+		return false;
+	}
 
-    @Override
-    public int getLoadedChunkCount()
-    {
-        return 0;
-    }
+	@Override
+	public int getLoadedChunkCount() {
+		return 0;
+	}
 
-    @Override
-    public boolean saveChunks(boolean p_73151_1_, IProgressUpdate progressCallback)
-    {
-        return true;
-    }
+	@Override
+	public boolean saveChunks(boolean p_73151_1_, IProgressUpdate progressCallback) {
+		return true;
+	}
 
-    @Override
-    // This is not implemented in any IChunkProvider in 1.8.9
-    public void saveExtraData()
-    {
-    }
+	@Override
+	// This is not implemented in any IChunkProvider in 1.8.9
+	public void saveExtraData() {
+	}
 
-    @Override
-    public boolean unloadQueuedChunks()
-    {
-        return false;
-    }
+	@Override
+	public boolean unloadQueuedChunks() {
+		return false;
+	}
 
-    @Override
-    public boolean canSave()
-    {
-        return true;
-    }
+	@Override
+	public boolean canSave() {
+		return true;
+	}
 
-    @Override
-    public String makeString()
-    {
-        return "RandomLevelSpace";
-    }
-    
-    @Override
-    public boolean chunkExists(int par1, int par2)
-    {
-        return true;
-    }
+	@Override
+	public String makeString() {
+		return "RandomLevelSpace";
+	}
+
+	@Override
+	public boolean chunkExists(int par1, int par2) {
+		return true;
+	}
 }
