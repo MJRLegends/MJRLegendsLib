23c87c8440d31face19ff6b44ae0a0313ba4bf31
MJRLegends <MJRLegends@users.noreply.github.com>
2018-03-07 04:11:55 +0000

    Fixed crash issue with changes to Axes

--- src/main/java/com/mjr/mjrlegendslib/item/BasicAxe.java
+++ src/main/java/com/mjr/mjrlegendslib/item/BasicAxe.java
@@ -5,7 +5,12 @@ import net.minecraft.item.ItemAxe;
 public class BasicAxe extends ItemAxe {
 
 	public BasicAxe(ToolMaterial material, String name) {
-		super(material);
+		super(material, material.getDamageVsEntity(), -3.0F);
+		this.setUnlocalizedName(name);
+	}
+
+	public BasicAxe(ToolMaterial material, String name, float damage, float speed) {
+		super(material, damage, speed);
 		this.setUnlocalizedName(name);
 	}
 }
