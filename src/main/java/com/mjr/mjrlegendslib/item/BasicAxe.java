af5e8f2997294e811800d4fad8d02741e9e340df
MJRLegends <MJRLegends@users.noreply.github.com>
2018-03-07 01:51:38 +0000

    Fixed issue #4

--- src/main/java/com/mjr/mjrlegendslib/item/BasicAxe.java
+++ src/main/java/com/mjr/mjrlegendslib/item/BasicAxe.java
@@ -1,19 +1,11 @@
 package com.mjr.mjrlegendslib.item;
 
-import java.util.Set;
+import net.minecraft.item.ItemAxe;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemTool;
-
-import com.google.common.collect.Sets;
-
-public class BasicAxe extends ItemTool {
-	private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(new Block[] { Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON,
-			Blocks.WOODEN_PRESSURE_PLATE });
+public class BasicAxe extends ItemAxe {
 
 	public BasicAxe(ToolMaterial material, String name) {
-		super(material, EFFECTIVE_ON);
+		super(material);
 		this.setUnlocalizedName(name);
 	}
 }
