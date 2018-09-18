f6444016d7a21cd03e10a902344c51201c75fc5a
MJRLegends <MJRLegends@users.noreply.github.com>
2018-09-18 21:37:38 +0100

    Added a ConfigGuiFactoryBase class

---
+++ src/main/java/com/mjr/mjrlegendslib/client/gui/ConfigGuiFactoryBase.java
@@ -0,0 +1,34 @@
+package com.mjr.mjrlegendslib.client.gui;
+
+import java.util.Set;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
+
+public abstract class ConfigGuiFactoryBase implements IModGuiFactory {
+
+	@Override
+	public void initialize(Minecraft minecraftInstance) {
+	}
+
+	@Override
+	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+		return null;
+	}
+
+	@Override
+	public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
+		return null;
+	}
+
+	@Override
+	public boolean hasConfigGui() {
+		return true;
+	}
+
+	@Override
+	public abstract GuiScreen createConfigGui(GuiScreen parentScreen);
+
+	public abstract Class<? extends GuiScreen> mainConfigGuiClass();
+}
