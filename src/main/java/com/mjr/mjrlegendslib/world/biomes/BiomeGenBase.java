81bb502a4d6e2a5d563ec54cbe08fe574e61b71c
MJRLegends <MJRLegends@users.noreply.github.com>
2019-02-19 19:34:51 +0000

    Comment out & Add Port 1.13 todo tag to them for porting later

--- src/main/java/com/mjr/mjrlegendslib/world/biomes/BiomeGenBase.java
+++ src/main/java/com/mjr/mjrlegendslib/world/biomes/BiomeGenBase.java
@@ -3,8 +3,8 @@ package com.mjr.mjrlegendslib.world.biomes;
 import net.minecraft.world.biome.Biome;
 
 public abstract class BiomeGenBase extends Biome {
-	public BiomeGenBase(BiomeProperties properties) {
-		super(properties);
+	public BiomeGenBase(Biome.BiomeBuilder biomeBuilder) {
+		super(biomeBuilder);
 	}
 
 	public void registerTypes() {
--- src/main/java/com/mjr/mjrlegendslib/world/features/WorldGenSphere.java
+++ src/main/java/com/mjr/mjrlegendslib/world/features/WorldGenSphere.java
@@ -1,50 +1,50 @@
-package com.mjr.mjrlegendslib.world.features;
-
-import java.util.Map.Entry;
-import java.util.Random;
-
-import com.mjr.mjrlegendslib.util.MessageUtilities;
-import com.mjr.mjrlegendslib.util.WorldGenShapeUtilities;
-import com.mjr.mjrlegendslib.util.WorldGenUtilities;
-
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.WorldGenerator;
-
-public class WorldGenSphere extends WorldGenerator {
-	private String modID;
-	private IBlockState state;
-	private int size;
-	private int yOffset;
-	private boolean showDebugInfo;
-
-	public WorldGenSphere(boolean showDebugInfo, String modID, IBlockState state, int size, int yOffset) {
-		super();
-		this.modID = modID;
-		this.state = state;
-		this.size = size;
-		this.yOffset = yOffset;
-		this.showDebugInfo = showDebugInfo;
-	}
-
-	@Override
-	public boolean generate(World world, Random rand, BlockPos position) {
-		if (WorldGenUtilities.checkValidSpawn(world, position, this.size) == false)
-			return false;
-		else {
-			if (this.showDebugInfo)
-				MessageUtilities.debugMessageToLog(modID, "Spawning Sphere at (x, y, z)" + position.toString());
-			generateStructure(world, rand, position.down(this.yOffset));
-		}
-		return true;
-	}
-
-	public boolean generateStructure(World world, Random rand, BlockPos position) {
-		for (Entry<BlockPos, IBlockState> temp : WorldGenShapeUtilities.generateSphere(this.state, this.size, position).entrySet()) {
-			world.setBlockState(temp.getKey(), temp.getValue(), 3);
-		}
-		return true;
-
-	}
-}
+//package com.mjr.mjrlegendslib.world.features;
+//
+//import java.util.Map.Entry;
+//import java.util.Random;
+//
+//import com.mjr.mjrlegendslib.util.MessageUtilities;
+//import com.mjr.mjrlegendslib.util.WorldGenShapeUtilities;
+//import com.mjr.mjrlegendslib.util.WorldGenUtilities;
+//
+//import net.minecraft.block.state.IBlockState;
+//import net.minecraft.util.math.BlockPos;
+//import net.minecraft.world.World;
+//import net.minecraft.world.gen.feature.WorldGenerator;
+//
+//public class WorldGenSphere extends WorldGenerator { TODO 1.13
+//	private String modID;
+//	private IBlockState state;
+//	private int size;
+//	private int yOffset;
+//	private boolean showDebugInfo;
+//
+//	public WorldGenSphere(boolean showDebugInfo, String modID, IBlockState state, int size, int yOffset) {
+//		super();
+//		this.modID = modID;
+//		this.state = state;
+//		this.size = size;
+//		this.yOffset = yOffset;
+//		this.showDebugInfo = showDebugInfo;
+//	}
+//
+//	@Override
+//	public boolean generate(World world, Random rand, BlockPos position) {
+//		if (WorldGenUtilities.checkValidSpawn(world, position, this.size) == false)
+//			return false;
+//		else {
+//			if (this.showDebugInfo)
+//				MessageUtilities.debugMessageToLog(modID, "Spawning Sphere at (x, y, z)" + position.toString());
+//			generateStructure(world, rand, position.down(this.yOffset));
+//		}
+//		return true;
+//	}
+//
+//	public boolean generateStructure(World world, Random rand, BlockPos position) {
+//		for (Entry<BlockPos, IBlockState> temp : WorldGenShapeUtilities.generateSphere(this.state, this.size, position).entrySet()) {
+//			world.setBlockState(temp.getKey(), temp.getValue(), 3);
+//		}
+//		return true;
+//
+//	}
+//}
