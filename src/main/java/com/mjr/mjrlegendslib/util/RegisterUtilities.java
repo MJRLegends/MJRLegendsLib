c9f5db8129be4c6838b4df77b63c2637defc6670
MJRLegends <MJRLegends@users.noreply.github.com>
2019-02-19 19:03:06 +0000

    Added new methods/made changes to RegisterUtilities class

--- src/main/java/com/mjr/mjrlegendslib/util/RegisterUtilities.java
+++ src/main/java/com/mjr/mjrlegendslib/util/RegisterUtilities.java
@@ -1,11 +1,21 @@
 package com.mjr.mjrlegendslib.util;
 
+import net.minecraft.block.BlockDispenser;
+import net.minecraft.dispenser.IBehaviorDispenseItem;
+import net.minecraft.util.IItemProvider;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeDictionary.Type;
 import net.minecraftforge.common.MinecraftForge;
 
 public class RegisterUtilities {
 
 	/*
 	 * public static void setHarvestLevel(String modID, Block block, String toolClass, int level) { TODO 1.13 block.setHarvestLevel(toolClass, level); }
+	 */
+
+	/*
+	 * public static void setHarvestLevel(Block block, String toolClass, int level, IBlockState state) {TODO 1.13 block.setHarvestLevel(toolClass, level, state); }
 	 */
 
 	public static void registerEventHandler(Object handler) {
@@ -17,4 +27,28 @@ public class RegisterUtilities {
 	 * 
 	 * public static void registerWorldGenerator(IWorldGenerator generator) { GameRegistry.registerWorldGenerator(generator, 0); }
 	 */
+
+	/*
+	 * public static void setFireBurn(Block block, int encouragement, int flammibility) {TODO 1.13 Blocks.FIRE.setFireInfo(block, encouragement, flammibility); }
+	 */
+
+	/*
+	 * public static void registerEntityPlacement(Class<? extends Entity> entity, SpawnPlacementType type) {TODO 1.13 EntitySpawnPlacementRegistry.setPlacementType(entity, type); }
+	 */
+
+	/*
+	 * public static void registerEndermanCarriable(Block block) {TODO 1.13 EntityEnderman.setCarriable(block, true); }
+	 */
+
+	public void registerProjectileDispense(IItemProvider item, IBehaviorDispenseItem projectile) {
+		BlockDispenser.registerDispenseBehavior(item, projectile);
+	}
+
+	/*
+	 * public static void registerForgeBucket(Fluid fluid) {TODO 1.13 FluidRegistry.addBucketForFluid(fluid); }
+	 */
+
+	public static void registerBiomeTypes(Biome biome, Type... types) {
+		BiomeDictionary.addTypes(biome, types);
+	}
 }
