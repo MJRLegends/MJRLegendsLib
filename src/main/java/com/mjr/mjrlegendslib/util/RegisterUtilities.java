60747ab320c8926b0e943a5f77250dfa26bf8f8b
MJRLegends <MJRLegends@users.noreply.github.com>
2019-09-16 00:08:14 +0100

    Added methods to RegisterUtilities that seem to got removed by accident

--- src/main/java/com/mjr/mjrlegendslib/util/RegisterUtilities.java
+++ src/main/java/com/mjr/mjrlegendslib/util/RegisterUtilities.java
@@ -1,6 +1,11 @@
 package com.mjr.mjrlegendslib.util;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import javax.annotation.Nonnull;
+
+import com.google.common.collect.ObjectArrays;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDispenser;
@@ -13,6 +18,7 @@ import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionType;
@@ -42,6 +48,39 @@ public class RegisterUtilities {
 		registerNonMobEntity(modID, mod, entityClass, name, 80, 3, true);
 		ResourceLocation resourcelocation = new ResourceLocation(modID, name);
 		EntityRegistry.registerEgg(resourcelocation, back, fore);
+	}
+	
+	public static void registerBlock(String modID, Block block, String name) {
+		GameRegistry.register(block.setRegistryName(modID, name));
+		GameRegistry.register(new ItemBlock(block).setRegistryName(modID, name));
+	}
+
+	public static void registerBlock(String modID, Block block, Class<? extends ItemBlock> itemclass, String name) throws NoSuchMethodException {
+		Object[] itemCtorArgs = new Object[] {};
+		ItemBlock i = null;
+		if (itemclass != null) {
+			Class<?>[] ctorArgClasses = new Class<?>[itemCtorArgs.length + 1];
+			ctorArgClasses[0] = Block.class;
+			for (int idx = 1; idx < ctorArgClasses.length; idx++) {
+				ctorArgClasses[idx] = itemCtorArgs[idx - 1].getClass();
+			}
+			Constructor<? extends ItemBlock> itemCtor = itemclass.getConstructor(ctorArgClasses);
+			try {
+				i = itemCtor.newInstance(ObjectArrays.concat(block, itemCtorArgs));
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException e) {
+				e.printStackTrace();
+			}
+		}
+		GameRegistry.register(block.setRegistryName(modID, name));
+		GameRegistry.register(i.setRegistryName(modID, name));
+	}
+
+	public static void registerItem(Item item) {
+		GameRegistry.register(item);
+	}
+
+	public static void registerItem(Item item, String name) {
+		GameRegistry.register(item.setRegistryName(name));
 	}
 
 	@SuppressWarnings("deprecation")
