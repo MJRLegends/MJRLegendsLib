83509b7dc5a065cd8846fab9a5cf3baaeb29e840
MJRLegends <MJRLegends@users.noreply.github.com>
2020-05-12 00:17:37 +0100

    Work work on porting

--- src/main/java/com/mjr/mjrlegendslib/inventory/IInventoryDefaults.java
+++ src/main/java/com/mjr/mjrlegendslib/inventory/IInventoryDefaults.java
@@ -1,47 +1,47 @@
-//package com.mjr.mjrlegendslib.inventory;
-//
-//import net.minecraft.entity.player.EntityPlayer;
-//import net.minecraft.inventory.IInventory;
-//import net.minecraft.util.text.ITextComponent;
-//import net.minecraft.util.text.TextComponentString;
-//import net.minecraft.util.text.TextComponentTranslation;
-//
-//public interface IInventoryDefaults extends IInventory {  TODO 1.13
-//	@Override
-//	public default void openInventory(EntityPlayer player) {
-//	}
-//
-//	@Override
-//	public default void closeInventory(EntityPlayer player) {
-//	}
-//
-//	@Override
-//	public default int getField(int id) {
-//		return 0;
-//	}
-//
-//	@Override
-//	public default void setField(int id, int value) {
-//	}
-//
-//	@Override
-//	public default int getFieldCount() {
-//		return 0;
-//	}
-//
-//	@Override
-//	public default void clear() {
-//
-//	}
-//
-//	// Override & return true if your using getName() method
-//	@Override
-//	public default boolean hasCustomName() {
-//		return false;
-//	}
-//
-//	@Override
-//	public default ITextComponent getDisplayName() {
-//		return (this.hasCustomName() ? new TextComponentString(this.getName()) : new TextComponentTranslation(this.getName(), new Object[0]));
-//	}
-//}
+package com.mjr.mjrlegendslib.inventory;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
+
+public interface IInventoryDefaults extends IInventory {
+	@Override
+	public default void openInventory(EntityPlayer player) {
+	}
+
+	@Override
+	public default void closeInventory(EntityPlayer player) {
+	}
+
+	@Override
+	public default int getField(int id) {
+		return 0;
+	}
+
+	@Override
+	public default void setField(int id, int value) {
+	}
+
+	@Override
+	public default int getFieldCount() {
+		return 0;
+	}
+
+	@Override
+	public default void clear() {
+
+	}
+
+	// Override & return true if your using getName() method
+	@Override
+	public default boolean hasCustomName() {
+		return false;
+	}
+
+	@Override
+	public default ITextComponent getDisplayName() {
+		return (this.hasCustomName() ? new TextComponentString(this.getName().getString()) : new TextComponentTranslation(this.getName().getString(), new Object[0]));
+	}
+}
--- src/main/java/com/mjr/mjrlegendslib/proxy/ClientProxy.java
+++ src/main/java/com/mjr/mjrlegendslib/proxy/ClientProxy.java
@@ -3,24 +3,27 @@ package com.mjr.mjrlegendslib.proxy;
 import com.mjr.mjrlegendslib.client.handlers.MainHandlerClient;
 import com.mjr.mjrlegendslib.util.RegisterUtilities;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.network.INetHandler;
+import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class ClientProxy implements IProxy {
-	
-	// TODO 1.13
-//	@Override
-//	public EntityPlayer getPlayerFromNetHandler(INetHandler handler) {
-//		if (handler instanceof NetHandlerPlayServer) {
-//			return ((NetHandlerPlayServer) handler).player;
-//		} else {
-//			return FMLClientHandler.instance().getClientPlayerEntity();
-//		}
-//	}
+
+	public EntityPlayer getPlayerFromNetHandler(INetHandler handler) {
+		if (handler instanceof NetHandlerPlayServer) {
+			return ((NetHandlerPlayServer) handler).player;
+		} else {
+			return Minecraft.getInstance().player;
+		}
+	}
 
 	@Override
 	public void setup(FMLCommonSetupEvent event) {
